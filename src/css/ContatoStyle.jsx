b4f5ef3898c991400088ada5455c74c2184d80cd
Andre <andrezicadobaile1@gmail.com>
2024-09-05 18:03:42 -0300

    terminando o contato

---
+++ src/css/ContatoStyle.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ContatoStyle =styled.section `
+
+    h1{
+   justify-content:center;
+    align-items:center;
+    display:flex;
+    background:#0f172a ;
+    color:white
+}
+
+h3 {
+    
+}
+
+`
--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,12 +1,20 @@
+import { ContatoStyle } from "../css/ContatoStyle"
+
 
 const Contato=()=>{
     return(
-        <>
+        <ContatoStyle>
         <div>
             <h1>Contato</h1>
-            <h2>Caso queira se comunicar conosco envie um email ou entre em contato pelo nosso numero 555-555-555</h2>
+            <img src="src/assets/banner.webp"></img>
+            <h2>Caso queira falar conosco temos nosso telefone 99975-4858
+                <tr>
+                Email: academialegal.hotmail.com
+                </tr>
+                Instagram: academialegal
+            </h2>
         </div>
-        </>
+        </ContatoStyle>
     )
 }
 export default Contato
