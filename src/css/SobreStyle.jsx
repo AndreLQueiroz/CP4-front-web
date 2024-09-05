08849c848aaaf7ab6a57c1be3d5fd4d7d5e863b5
Andre <andrezicadobaile1@gmail.com>
2024-09-05 18:13:59 -0300

    adiciondo sobre

---
+++ src/css/SobreStyle.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const SobreStyle =styled.section `
+
+h1{
+   justify-content:center;
+    align-items:center;
+    display:flex;
+    background:#0f172a ;
+    color:white
+}
+.imga {
+    align-items:  center;
+    justify-content: center;
+    display: flex;
+}
+`
--- src/routes/Sobre.jsx
+++ src/routes/Sobre.jsx
@@ -1,16 +1,52 @@
+import { SobreStyle } from "../css/SobreStyle"
+
+
+
 const Sobre=()=>{
     return(
-        <>
+        <SobreStyle>
         <div>
             <h1>Sobre nos</h1>
-            <img src='src/assets/img1.jpg' alt='Sobre nos'/>
-            <p>Treinar regularmente é essencial para manter a saúde e o bem-estar em dia. Quando dedicamos tempo ao exercício físico, não só promovemos o fortalecimento muscular e a melhoria da resistência cardiovascular, mas também contribuímos para a saúde mental. A prática de atividades físicas libera endorfinas, conhecidas como hormônios da felicidade, que ajudam a reduzir o estresse e a ansiedade. Além disso, o treino regular pode melhorar a qualidade do sono, aumentar a autoestima e promover uma sensação geral de bem-estar.
+            <img src='src/assets/img1.jpg' alt='Sobre nos' width="1700px" height="600px"/>
+            <p>Chegou a hora de colocar aquela roupa de treino, calçar aquele tênis, deixar a preguiça de lado e correr para a academia! São inúmeras as motivações para se começar uma rotina de hábitos saudáveis, e a atividade física vem como facilitadora deste processo.
+            <tr>
+Se você é aquela pessoa que sempre tem um motivo para não sair desse sofá, segue 6 motivos importantes que vão te fazer ir para a academia hoje.
+</tr>
+1. Aumento da qualidade de vida
+Todo mundo sabe que é importante para a saúde se manter ativo e permanecer em uma rotina de treinamento.
+<tr>
+Mas, mesmo assim, a grande parte da população mundial permanece sedentária, por esse motivo segue alguns benefícios da atividade física para a manutenção da saúde:
 
-Outro benefício importante do treinamento é o aumento da disposição e da energia. Ao integrar o exercício na rotina diária, o corpo se torna mais eficiente no uso da energia, o que pode levar a um aumento significativo na produtividade e na motivação para realizar tarefas do dia a dia. A prática de exercícios também desempenha um papel crucial na prevenção de doenças crônicas, como diabetes e hipertensão, ajudando a manter o corpo saudável e funcionando de maneira ideal.
+Melhora do condicionamento muscular e cardiorrespiratório;
+Fortalece ossos e músculos;
+Redução do risco de doenças cardiovasculares, diabetes, osteoporose, fibromialgia, alguns cânceres, entre outras doenças;
+Melhora da saúde mental e do humor;
+Controla o peso;
+Reduz o estresse;
+Controla a pressão arterial e níveis de açúcar no sangue;
+Aumento da expectativa de vida;
+Manutenção da autonomia e funcionalidade nas atividades diárias e prevenção do risco de quedas para pessoas de terceira idade.
+Melhora da qualidade do sono;
+</tr>
+2. Mais disposição
+A liberação de hormônios após a prática do exercício nos deixa mais ativo, nos deixa mais disposto e regula as principais ações fisiológicas do nosso corpo, até o sono muda completamente, tornando um excelente motivo para aquela pessoa que tem uma rotina intensa de trabalho.
+<tr>
+3. Socialização
+A academia é o melhor lugar do mundo para conhecer pessoas e para encontrar amigos. Fazer novas amizades na academia só traz benefícios para a sua saúde. Treinar com alguém por perto faz toda a diferença, um acaba motivando o outro e consequentemente os dois acabam alcançando rapidamente o objetivo.
 
-Por último, treinar pode ser uma excelente maneira de socializar e fazer novas amizades, especialmente em ambientes como academias ou grupos de esportes. Estabelecer conexões com outras pessoas que compartilham interesses semelhantes pode proporcionar uma rede de apoio e motivação adicional. Portanto, investir tempo e esforço no treinamento físico é um passo fundamental para uma vida mais saudável e equilibrada.</p>
+[rock-convert-cta id=”7789″]
+</tr>
+4. Otimização de Tempo
+Existem academias que com amplas parcerias de serviços que nos ajudam a ganhar tempo, que contam com restaurantes, salão de beleza, espaço para crianças, loja de suplementos e algumas possuem até lavanderias. Você pode resolver assuntos pessoais, cuidar da sua saúde, tudo em um só lugar.
+<tr>
+5. Treino com especialistas
+O professor de Educação física na academia tem o dever de avaliar, prescrever, orientar e supervisionar um programa de treinamento individualizado para o aluno. Ele considera as principais necessidades e limitações de cada um, respeitando os princípios de treinamento para que o indivíduo consiga chegar ao seu objetivo.
+</tr>
+6. Aulas coletivas
+As aulas coletivas são excelentes estratégias para quem gosta de atividades mais dinâmicas, diversificadas e em grupo, com grande intensidade de motivação. São aulas com estímulos variados, que trabalham todo o corpo, com música e professores motivantes, existem para todos os níveis e objetivos.m ambientes como academias ou grupos de esportes. Estabelecer conexões com outras pessoas que compartilham interesses semelhantes pode proporcionar uma rede de apoio e motivação adicional. Portanto, investir tempo e esforço no treinamento físico é um passo fundamental para uma vida mais saudável e equilibrada.</p>
+       
         </div>
-        </>
+        </SobreStyle>
     )
 }
 export default Sobre
